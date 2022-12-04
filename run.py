0dddf67244b7b61c42cc9e19e4750fe2a2261fdf
Dima Doroshev <dima@doroshev.com>
2022-12-04 13:23:32 +0200

    refactor

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python
+
 import importlib
 import sys
 
 
 if __name__ == '__main__':
     day = sys.argv[1]
-    step = sys.argv[2]
-    module = importlib.import_module(f'd{day}.s{step}')
+    part = sys.argv[2]
+    module = importlib.import_module(f'd{day}.p{part}')
     with open(f'd{day}/input.txt') as f:
         res = module.run(map(str.strip, f))
         print(res)
